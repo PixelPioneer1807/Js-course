4832db09778a27062624aca1567e948e3d2ada22
Shivam Srivastava <159549025+PixelPioneer1807@users.noreply.github.com>
2025-07-25 07:47:30 +0000

    Lecture 1 and 2

---
+++ l1.js
@@ -0,0 +1,11 @@
+const a=10
+let b=20
+var c=30
+d=40  /*Bad practice*/
+console.table([a,b,c,d])
+
+// we don't use var because of the issue in block scope 
+
+let e;
+console.log(e);  /*This prints undefined */
+
---
+++ l2.js
@@ -0,0 +1,15 @@
+// Number ->
+let a=10
+// String 
+let b="Shivam"
+// Boolean 
+let c=true
+// Null 
+let d=null
+// undefined 
+let e;
+// big int is used for greated range of number 
+// symbols are used in react mostly when difining uniqueness 
+console.log(typeof(null));  /* This is an object we'll discuss more about this in other lectures8*/
+
+
